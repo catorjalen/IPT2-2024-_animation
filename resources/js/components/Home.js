7bbeeca3061c570f992e7beab8f1ed0b84eaf7d7
catorjalen <cator.jalen@gmail.com>
2024-10-07 15:50:22 +0800

    nuw commit

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-export default function home() {
+export default function Home() {
     return (
         <div className="home-container">
             <Navbar />
