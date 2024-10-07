1fc26110e6ec4794a9521510bceaf5cb212a238a
catorjalen <cator.jalen@gmail.com>
2024-10-07 15:46:48 +0800

    new

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-export default function Home() {
+export default function home() {
     return (
         <div className="home-container">
             <Navbar />
